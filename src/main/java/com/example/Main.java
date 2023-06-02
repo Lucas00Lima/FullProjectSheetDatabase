2af483d42b068d913376630a109cf9933dbebed5
Lima <117579221+Lucas00Lima@users.noreply.github.com>
2023-06-02 20:14:25 -0300

    Inicio

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         // Sistema acessa o banco de dados e pegas as colunas setando cada coluna
         Database database = new Database();
-
         database.connectionDatabase();
+
+        
     }
 }
