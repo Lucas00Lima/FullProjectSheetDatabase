1323f04237564e4f0129158def851e77ca03ae55
Lima <lukinha-lenta@hotmail.com>
2023-06-02 20:12:04 -0300

    Inicio

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        // Sistema acessa o banco de dados e pegas as colunas setando cada coluna
+        Database database = new Database();
+
+        database.connectionDatabase();
+    }
+}
