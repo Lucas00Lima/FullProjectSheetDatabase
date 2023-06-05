46a75aa9a1f2bfe87cab361ebfac64f54b4e2de8
Lima <lukinha-lenta@hotmail.com>
2023-06-05 18:21:24 -0300

    Create SheetAcess

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
+import org.apache.poi.EncryptedDocumentException;
+
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, EncryptedDocumentException, IOException {
         // Sistema acessa o banco de dados e pegas as colunas setando cada coluna
         Database database = new Database();
         database.connectionDatabase();
--- src/main/java/com/example/SheetAcess.java
+++ src/main/java/com/example/SheetAcess.java
@@ -1,18 +1,20 @@
 package com.example;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
+import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.*;
 
 public class SheetAcess {
     private String filepath;
-    private String rowIndex;
+    private Integer rowIndex;
     private Integer totalLinhasInseridas;
 
-    public void acessSheet() {
+    public void acessSheet() throws EncryptedDocumentException, IOException {
         rowIndex = null;
         totalLinhasInseridas = 0;
         JFileChooser fileChooser = new JFileChooser();
@@ -20,13 +22,15 @@ public class SheetAcess {
         FileInputStream fileInputStream = new FileInputStream(filepath);
         Workbook Workbook = WorkbookFactory.create(fileInputStream);
         Sheet sheet = Workbook.getSheetAt(0);
-        
+        DataFormatter dataFormatter = new DataFormatter();
 
         if (result == JFileChooser.APPROVE_OPTION) {
             filepath = fileChooser.getSelectedFile().getAbsolutePath();
             JOptionPane.showMessageDialog(null, "Aquivo selecionado" + filepath);
         }
-        for (rowIndex = 1; rowIndex <= )
+        for (rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+            
+        }
 
     }
 }
