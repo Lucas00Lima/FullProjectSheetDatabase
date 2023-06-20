85b241f6324cc2f00653d1b8538c61ab3e12d6ac
Lucas00Lima <lukinha-lenta@hotmail.com>
2023-06-19 23:36:01 -0300

    --no-editor

--- src/main/java/com/example/Query.java
+++ src/main/java/com/example/Query.java
@@ -1,17 +1,20 @@
 package com.example;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import org.apache.poi.ss.usermodel.Workbook;
 
 public class Query {
 	private Connection connection;
 
-	public void query() throws SQLException {
+	public void query() throws SQLException, FileNotFoundException {
 		Database data = new Database();
 		try (Connection connection = data.connectionDatabase()) {
-
+			
 		}
-
 	}
-
 }
--- src/main/java/com/example/SheetAcess.java
+++ src/main/java/com/example/SheetAcess.java
@@ -3,9 +3,6 @@ package com.example;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -13,21 +10,24 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class SheetAcess {
 	private String filePath;
-    public void acessSheet() throws EncryptedDocumentException, IOException {
-    	String filePath = "C:\\Users\\lukin\\OneDrive\\Área de Trabalho\\Planilha\\usaressa.xlsx";
-    }
 
-	/*
-	 * JFileChooser fileChooser = new JFileChooser(); int result =
-	 * fileChooser.showOpenDialog(null); if (result == JFileChooser.APPROVE_OPTION)
-	 * { filePath = fileChooser.getSelectedFile().getAbsolutePath();
-	 * JOptionPane.showMessageDialog(null, "Aquivo selecionado " + getFilePath()); }
-	 * FileInputStream fileInputStream = new FileInputStream(getFilePath());
-	 * Workbook workbook = WorkbookFactory.create(fileInputStream); Sheet sheet =
-	 * workbook.getSheetAt(0); }
-	 */
+	public void acessSheet() throws EncryptedDocumentException, IOException {
+		String filePath = "C:\\Users\\lukin\\OneDrive\\Área de Trabalho\\Planilha\\usaressa.xlsx";
+
+//		JFileChooser fileChooser = new JFileChooser();
+//		int result = fileChooser.showOpenDialog(null);
+//		if (result == JFileChooser.APPROVE_OPTION) {
+//			filePath = fileChooser.getSelectedFile().getAbsolutePath();
+//			JOptionPane.showMessageDialog(null, "Aquivo selecionado " + getFilePath());
+//		}
+		FileInputStream fileInputStream = new FileInputStream(filePath);
+		Workbook workbook = WorkbookFactory.create(fileInputStream);		
+		Sheet sheet = workbook.getSheetAt(0);
+		System.out.println(filePath);
+	}
+
 	public String getFilePath() {
 		return filePath;
 	}
-}
 
+}
